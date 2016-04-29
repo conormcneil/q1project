9d1e32c67d275418d2629493675aec581c4eef3d
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-04-29 15:34:38 -0600

    added userStory.txt

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   console.log("I'm ready!");
   $.ajax({
     url: "https://api.propublica.org/campaign-finance/v1?api_key=4jVRBAKrhn4nniRoSo5Gf4AWuM8DaA9G3GUC9pqN",
+    headers: {'Access-Control-Allow-Origin': '*'},
     method: 'GET',
     success: function(data) {
       console.log(data);
