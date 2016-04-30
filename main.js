920a2c6c9dfd12d2406b9b8313980933b1625436
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-04-29 21:34:17 -0600

    updated userStory.txt and began agile workflow

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+// function myCallback(data) {
+//   console.log(data);
+// }
+
 $(document).ready(function() {
   console.log("I'm ready!");
   $.ajax({
-    url: "https://api.propublica.org/campaign-finance/v1?api_key=4jVRBAKrhn4nniRoSo5Gf4AWuM8DaA9G3GUC9pqN",
-    headers: {'Access-Control-Allow-Origin': '*'},
+    url: "http://www.opensecrets.org/api/?method=candContrib&cid=N00007360&cycle=2016&apikey=d52a49dc099032a4d6cdb4eb74802be2",
     method: 'GET',
+    origin: 'http://localhost:8080',
     success: function(data) {
       console.log(data);
     }
