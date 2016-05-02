c4c18d1f965e8dc0d6fd65c3cb4aa6e7453b8cc3
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-05-02 12:24:06 -0600

    committing changes before wrecking tables

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// function myCallback(data) {
-//   console.log(data);
-// }
-
 $(document).ready(function() {
   console.log("I'm ready!");
   // $.ajax({
@@ -17,15 +13,15 @@ $(document).ready(function() {
   var statesAbbr = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 
   // Use function to populate table of states
-  var content = $('.content');
-  content.append("<table class='states'></table>");
-  var mainTable = $("table");
-  var makeStatesTable = function(arr) {
-    for (var i = 0; i < arr.length; i++) {
-      mainTable.append("<tr id=" + statesAbbr[i] + "><td>" + arr[i] + "</td></tr>");
-    }
-  }
-  makeStatesTable(states);
+  // var content = $('.content');
+  // content.append("<table class='states'></table>");
+  // var mainTable = $("table");
+  // var makeStatesTable = function(arr) {
+  //   for (var i = 0; i < arr.length; i++) {
+  //     mainTable.append("<tr id=" + statesAbbr[i] + "><td>" + arr[i] + "</td></tr>");
+  //   }
+  // }
+  // makeStatesTable(states);
 
   // Create click events on state names
   $("td").click(function() {
@@ -33,9 +29,24 @@ $(document).ready(function() {
     // var activeStateAbbr = ''; Set abbr at the same time; useful for data parsing w API?
     activeState = event.target.innerHTML;
     console.log(activeState);
+  });
+
+
+  // Toggle House/Senate view on state click
+  $(".tdHide").hide();
+  $("table").click(function(event) {
+    event.stopPropagation();
+    var $target = $(event.target);
+    if ($target.closest("td").hasClass("tdHide")) {
+      $target.slideUp('fast');
+    } else {
+      $target.closest("tr").next().find("td").slideToggle();
+    }
+    console.log(event.target);
+    $("p").click(function(event) {
+      console.log(event.target);
+    })
   })
-
-
 
 
 })
