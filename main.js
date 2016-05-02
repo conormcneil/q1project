54b0dbe44d63f0b62e031aa3cf8d0f2f787bacc5
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-05-02 12:38:58 -0600

    removed tables

--- main.js
+++ main.js
@@ -12,16 +12,17 @@ $(document).ready(function() {
   var states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Deleware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
   var statesAbbr = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 
-  // Use function to populate table of states
-  // var content = $('.content');
-  // content.append("<table class='states'></table>");
-  // var mainTable = $("table");
-  // var makeStatesTable = function(arr) {
-  //   for (var i = 0; i < arr.length; i++) {
-  //     mainTable.append("<tr id=" + statesAbbr[i] + "><td>" + arr[i] + "</td></tr>");
-  //   }
-  // }
-  // makeStatesTable(states);
+  // Use function to display states
+  var content = $('.content');
+  content.append("<section class='states'></section>");
+  var stateList = $("section");
+  var displayStates = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      stateList.append("<article class='state'>" + arr[i] + "</article>");
+      stateList.append("<p>House</p><p>Senate</p>");
+    }
+  }
+  displayStates(states);
 
   // Create click events on state names
   $("td").click(function() {
@@ -33,20 +34,20 @@ $(document).ready(function() {
 
 
   // Toggle House/Senate view on state click
-  $(".tdHide").hide();
-  $("table").click(function(event) {
-    event.stopPropagation();
-    var $target = $(event.target);
-    if ($target.closest("td").hasClass("tdHide")) {
-      $target.slideUp('fast');
-    } else {
-      $target.closest("tr").next().find("td").slideToggle();
-    }
-    console.log(event.target);
-    $("p").click(function(event) {
-      console.log(event.target);
-    })
-  })
+  // $(".tdHide").hide();
+  // $("table").click(function(event) {
+  //   event.stopPropagation();
+  //   var $target = $(event.target);
+  //   if ($target.closest("td").hasClass("tdHide")) {
+  //     $target.slideUp('fast');
+  //   } else {
+  //     $target.closest("tr").next().find("td").slideToggle();
+  //   }
+  //   console.log(event.target);
+  //   $("p").click(function(event) {
+  //     console.log(event.target);
+  //   })
+  // })
 
 
 })
