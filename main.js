3691919b408d3063a5a03dee88662f3502ab8a22
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-05-02 13:21:58 -0600

    working on toggling House/Senate views

--- main.js
+++ main.js
@@ -19,35 +19,20 @@ $(document).ready(function() {
   var displayStates = function(arr) {
     for (var i = 0; i < arr.length; i++) {
       stateList.append("<article class='state'>" + arr[i] + "</article>");
-      stateList.append("<p>House</p><p>Senate</p>");
+      stateList.append("<aside><p>House</p><p>Senate</p></aside>");
     }
   }
   displayStates(states);
 
   // Create click events on state names
-  $("td").click(function() {
+  $("aside").addClass('hide');
+  $("article").click(function(event) {
     var activeState = '';
-    // var activeStateAbbr = ''; Set abbr at the same time; useful for data parsing w API?
+    event.stopPropagation();
     activeState = event.target.innerHTML;
     console.log(activeState);
+    // Toggle House/Senate view on state click
+    $(this).children("aside").toggleClass('hide');
+
   });
-
-
-  // Toggle House/Senate view on state click
-  // $(".tdHide").hide();
-  // $("table").click(function(event) {
-  //   event.stopPropagation();
-  //   var $target = $(event.target);
-  //   if ($target.closest("td").hasClass("tdHide")) {
-  //     $target.slideUp('fast');
-  //   } else {
-  //     $target.closest("tr").next().find("td").slideToggle();
-  //   }
-  //   console.log(event.target);
-  //   $("p").click(function(event) {
-  //     console.log(event.target);
-  //   })
-  // })
-
-
 })
