d6baf51e901dbfaef1b4f33f7a8befb177428675
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-05-03 16:50:58 -0600

    colored each candidate by party

--- main.js
+++ main.js
@@ -26,20 +26,28 @@ $(document).ready(function() {
       method: 'GET',
       origin: 'http://localhost:8080',
       success: function(data) {
+        console.log(data);
         $("#incumbentView").append("<p style='font-size: 1.5em'>Incumbent Candidate(s): 2016</p>")
         for (var i = 0; i < data.results.length; i++) {
-          $("#incumbentView").append('<p id="' + data.results[i].candidate_id + '">' + data.results[i].name + '</p>');
+            $("#incumbentView").append('<aside class="' + data.results[i].party + '">' + data.results[i].name + ' (' + data.results[i].party + ')</aside>');
+
+
+          // Use candidate_id to retrieve that candidate's committee_ids
           $.ajax({
             url: "https://api.open.fec.gov/v1/candidate/" + data.results[i].candidate_id + "/committees/?sort=name&per_page=100&page=1&api_key=" + apiKey,
             method: 'GET',
             origin: 'http://localhost:8080',
             success: function(moreData) {
               for (var i = 0; i < moreData.results.length; i++) {
+
+                // use each committee_id to request filing info for that committee
                 $.ajax({
                   url: "https://api.open.fec.gov/v1/committee/" + moreData.results[i].committee_id + "/filings/?page=1&sort=-receipt_date&cycle=2016&report_year=2016&per_page=100&api_key=" + apiKey,
                   method: 'GET',
                   origin: 'http://localhost:8080',
                   success: function(bestData) {
+
+                    // if committee_id filings show any expenditures, add them here
                     if (bestData.results.length > 0) {
                       for (var i = 0; i < bestData.results.length; i++) {
                         counter += bestData.results[i].total_disbursements;
@@ -59,41 +67,55 @@ $(document).ready(function() {
     }); // End ajax req: 1; incumbents
 
     // Get Challengers Filing Info
-    $.ajax({
-      url: "https://api.open.fec.gov/v1/candidates/?page=1&sort=state&candidate_status=C&incumbent_challenge=C&state=" + $state + "&office=" + chamber + "&per_page=100&api_key=" + apiKey,
-      method: 'GET',
-      origin: 'http://localhost:8080',
-      success: function(data) {
-        $("#challengerView").append("<p style='font-size: 1.5em'>Challenger(s): 2016</p>")
-        for (var i = 0; i < data.results.length; i++) {
-          $("#challengerView").append('<p id="' + data.results[i].candidate_id + '">' + data.results[i].name + '</p>');
-          $.ajax({
-            url: "https://api.open.fec.gov/v1/candidate/" + data.results[i].candidate_id + "/committees/?sort=name&per_page=100&page=1&api_key=" + apiKey,
-            method: 'GET',
-            origin: 'http://localhost:8080',
-            success: function(moreData) {
-              for (var i = 0; i < moreData.results.length; i++) {
-                $.ajax({
-                  url: "https://api.open.fec.gov/v1/committee/" + moreData.results[i].committee_id + "/filings/?page=1&sort=-receipt_date&cycle=2016&report_year=2016&per_page=100&api_key=" + apiKey,
-                  method: 'GET',
-                  origin: 'http://localhost:8080',
-                  success: function(bestData) {
-                    if (bestData.results.length > 0) {
-                      for (var i = 0; i < bestData.results.length; i++) {
-                        counter += bestData.results[i].total_disbursements;
-                      }
-                    } else {
-                      console.log('no filing data for this committee');
-                    }
-                    var counterFixed = counter.toFixed(2);
-                    $("#challengerView").prepend($("#challengerFinance").html("$" + counterFixed));
-                  }
-                })
-              }
-            }
-          });
-        }
-      }
-    }); // End ajax req: 2; challengers
+    // $.ajax({
+    //   url: "https://api.open.fec.gov/v1/candidates/?page=1&sort=state&candidate_status=C&incumbent_challenge=C&state=" + $state + "&office=" + chamber + "&per_page=100&api_key=" + apiKey,
+    //   method: 'GET',
+    //   origin: 'http://localhost:8080',
+    //   success: function(data) {
+    //     console.log(data);
+    //     $("#challengerView").append("<p style='font-size: 1.5em'>Challenger(s): 2016</p>")
+    //     for (var i = 0; i < data.results.length; i++) {
+    //       $("#challengerView").append('<p id="' + data.results[i].candidate_id + '">' + data.results[i].name + ' (' + data.results[i].party + ')</p>');
+    //
+    //       // Use candidate_id to retrieve that candidate's committee_ids
+    //       $.ajax({
+    //         url: "https://api.open.fec.gov/v1/candidate/" + data.results[i].candidate_id + "/committees/?sort=name&per_page=100&page=1&api_key=" + apiKey,
+    //         method: 'GET',
+    //         origin: 'http://localhost:8080',
+    //         success: function(moreData) {
+    //           for (var i = 0; i < moreData.results.length; i++) {
+    //             if (data.results[i].party === "REP") {
+    //               $("#challengerView").append('<aside class="' + data.results[i].party + '">' + data.results[i].name + ' (' + data.results[i].party + ')</aside>');
+    //             } else if (data.results[i].party === "DEM") {
+    //               $("#challengerView").append('<aside class="' + data.results[i].party + '">' + data.results[i].name + ' (' + data.results[i].party + ')</aside>');
+    //             } else {
+    //               $("#challengerView").append('<aside class="' + data.results[i].party + '">' + data.results[i].name + ' (' + data.results[i].party + ')</aside>');
+    //             }
+    //
+    //             // use each committee_id to request filing info for that committee
+    //             $.ajax({
+    //               url: "https://api.open.fec.gov/v1/committee/" + moreData.results[i].committee_id + "/filings/?page=1&sort=-receipt_date&cycle=2016&report_year=2016&per_page=100&api_key=" + apiKey,
+    //               method: 'GET',
+    //               origin: 'http://localhost:8080',
+    //               success: function(bestData) {
+    //
+    //                 // if committee_id filings show any expenditures, add them here
+    //                 if (bestData.results.length > 0) {
+    //                   for (var i = 0; i < bestData.results.length; i++) {
+    //                     counter += bestData.results[i].total_disbursements;
+    //                   }
+    //                 } else {
+    //                   console.log('no filing data for this committee');
+    //                 }
+    //                 var counterFixed = counter.toFixed(2);
+    //                 $("#challengerView").prepend($("#challengerFinance").html("$" + counterFixed));
+    //               }
+    //             });
+    //           }
+    //         }
+    //       });
+    //     }
+    //   }
+    // }); // End ajax req: 2; challengers
   })
 })
